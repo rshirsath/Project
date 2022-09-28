f19babe7282cdb5800d23ca9cc3f51b6106ac400
rshirsath <113876481+rshirsath@users.noreply.github.com>
2022-09-28 10:19:06 +0530

    Add files via upload

---
+++ ternary3.java
@@ -0,0 +1,14 @@
+//wap to check if given number is positive or negative 
+
+class ternary3
+{
+
+public static void main(String a1[])
+{
+int number=2
+
+String result = (number<0) ? "Negative" : "positive";
+
+System.out.println(result);
+
+}}
